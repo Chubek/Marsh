07979b59a02c2f7ce31b401d52a15e4c5b51d056
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-20 16:43:45 +0330

    Init & Added: initializd word expansion file, added basic declarations with placeholders

---
+++ word-expansion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <fnmatch.h>
+#include <wordexp.h>
+
+typedef struct ShellWord {
+	// implement
+} ShellWord;
+
+
+typedef struct GlobPattern {
+	// implement
+} GlobPattern;
+
+
+
+ShellWord *create_shell_word(char *word) {
+	// implement
+}
+
+
+GlobPattern *create_glob_pattern(char *word) {
+	// implement
+}
+
+
