2ae1d74c8f13dd501e7f6f32e261cf7d1e32953f
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-26 21:38:21 +0330

    Modularized: Header and Body File for job-control.c

--- job-control.c
+++ job-control.c
@@ -12,6 +12,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include "marsh.h"
+
 typedef enum {
   PSTATE_PENDING,
   PSTATE_RUNNING,
@@ -44,25 +46,6 @@ typedef struct Job {
 static struct termios shell_tmodes;
 static Job *jobs = NULL;
 static pid_t shell_pgid = -1;
-
-void initialize_job_control(void);
-void sigchld_handler(int sig);
-void sigint_handler(int sig);
-void sigstp_handler(int sig);
-void sigcont_handler(int sig);
-void setup_signal_handlers(void);
-void launch_job(Job *job, int foreground);
-void put_job_in_foreground(Job *job, int cont);
-void put_job_in_background(Job *job, int cont);
-void wait_for_job(Job *job);
-bool job_is_stopped(Job *job);
-bool job_is_completed(Job *job);
-void update_status(void);
-void mark_process_status(pid_t pid, int status);
-Job *create_job(void);
-Process *create_process(char **argv, int argc);
-void append_process_to_job(Job *job, Process *process);
-void append_job_to_list(Job *new_job);
 
 void initialize_job_control(void) {
   pid_t pid = getpid();
---
+++ marsh.h
@@ -0,0 +1,31 @@
+#ifndef MARSH_H
+#define MARSH_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Process Process;
+typedef struct Job Job;
+
+void initialize_job_control(void);
+void sigchld_handler(int sig);
+void sigint_handler(int sig);
+void sigstp_handler(int sig);
+void sigcont_handler(int sig);
+void setup_signal_handlers(void);
+void launch_job(Job *job, int foreground);
+void put_job_in_foreground(Job *job, int cont);
+void put_job_in_background(Job *job, int cont);
+void wait_for_job(Job *job);
+bool job_is_stopped(Job *job);
+bool job_is_completed(Job *job);
+void update_status(void);
+void mark_process_status(pid_t pid, int status);
+Job *create_job(void);
+Process *create_process(char **argv, int argc);
+void append_process_to_job(Job *job, Process *process);
+void append_job_to_list(Job *new_job);
+
+#endif
