79155bc1306ca116407621e13d81148251bb48b6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-28 23:46:59 +0330

    Added: basic data structures for shell environment

---
+++ environ.c
@@ -0,0 +1,59 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "marsh.h"
+
+typedef enum {
+  PSTAT_PENDING,
+  PSTAT_RUNNING,
+  PSTAT_WAITING,
+  PSTAT_COMPLETE,
+  PSTAT_TERMINATED,
+} pstat_t;
+
+struct Stdio {
+  int fno_in;
+  int fno_out;
+  int fno_err;
+};
+
+struct Command {
+  char *util_name;
+  char **arguments;
+  size_t num_arguments;
+  Stdio *io;
+};
+
+struct Process {
+  pid_t self_id;
+  pid_t group_id;
+  Command *command;
+  bool async;
+  bool piped;
+  pstate_t status;
+  Process *next;
+};
+
+struct Job {
+  pid_t group_id;
+  bool foreground;
+  struct termios tmodes;
+  Process *first_process;
+  Job *next;
+};
+
+struct Control {
+  Job *foreground_job;
+  Job **background_jobs;
+  FDescTbl *fd_table;
+  NameTbl *name_table;
+  FunTbl *func_table;
+  bool interactive;
+};
