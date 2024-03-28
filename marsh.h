f72c40b2875ddb0953cdaecf1e91e57cc6f9f742
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-28 23:47:58 +0330

    Added: basic data structures declarations

--- marsh.h
+++ marsh.h
@@ -6,34 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Stdio Stdio;
+typedef struct Command Command;
 typedef struct Process Process;
 typedef struct Job Job;
-
-void initialize_job_control(void);
-void sigchld_handler(int sig);
-void sigint_handler(int sig);
-void sigstp_handler(int sig);
-void sigcont_handler(int sig);
-void setup_signal_handlers(void);
-void launch_job(Job *job, int foreground);
-void put_job_in_foreground(Job *job, int cont);
-void put_job_in_background(Job *job, int cont);
-void wait_for_job(Job *job);
-bool job_is_stopped(Job *job);
-bool job_is_completed(Job *job);
-void update_status(void);
-void mark_process_status(pid_t pid, int status);
-Job *create_job(void);
-Process *create_process(char **argv, int argc);
-void append_process_to_job(Job *job, Process *process);
-void append_job_to_list(Job *new_job);
-void dump_process_list(Process *head);
-void dump_job_list(Job *job);
-void collect_on_job_list(void);
-void dump_job(Job *job);
-void dump_all_jobs(void);
-
-typedef struct Symdef Symdef;
-typedef struct Symtbl Symtbl;
+typedef struct Environ Environ;
 
 #endif
