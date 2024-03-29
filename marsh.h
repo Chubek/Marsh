2e88b4306d2424e5d7ef90fcb4221eb555a1ea11
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-29 20:12:27 +0330

    Symbols Moved: Symbols moved from tables.c

--- marsh.h
+++ marsh.h
@@ -1,12 +1,30 @@
 #ifndef MARSH_H
 #define MARSH_H
 
+// A. Global Symbolic Macro Definitions
+
+#ifndef MAX_BUCKET
+#define MAX_BUCKET 4096
+#endif
+  
+#ifndef MAX_CACHE
+#define MAX_CACHE 1024
+#endif
+
+#ifndef MAX_IDENT
+#define MAX_IDENT 256
+#endif
+
+#ifndef FILEPATH_MAX
+#define FILEPATH_MAX (FILENAME_MAX * 16)
+#endif
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// A. Shell environment datatypes
+// B. Shell environment datatypes
 
 typedef struct FDescState FDescState;
 typedef struct Redirection Redirection;
@@ -15,7 +33,7 @@ typedef struct Process Process;
 typedef struct Job Job;
 typedef struct Environ Environ;
 
-// B. Shell tables datatyps
+// C. Shell tables datatyps
 
 typedef struct FDescTblNode FDescTblNode;
 typedef struct FDescTbl FDescTbl;
@@ -24,12 +42,14 @@ typedef struct SymTbl SymTbl;
 typedef struct FuncTblNode FuncTblNode;
 typedef struct FuncTbl FuncTbl;
 
-// Function Prototypes for  Redirection Factory Functions
+// D. Environment Definition Factory Function Declarations
+
+/* Function Prototypes for  Redirection Factory Functions */
 Redirection *create_redirection(redirflags_t flags, char *target,
                                 bool target_fdesc, int fdesc);
 Redirection *duplicate_redir(Redirection *redir);
 
-// Function Prototypes for Process & Command Factory Functions
+/* Function Prototypes for Process & Command Factory Functions */
 Command *create_command(char *util_name, char **arguments,
                         size_t num_arguments);
 Command *duplicate_command(Command *command);
@@ -40,7 +60,7 @@ void push_next_process(Process *head, Process *next);
 void set_process_self_id(Process *process, pid_t self_id);
 void set_process_group_id(Process *process, pid_t group_id);
 
-// Function Prototypes for Job & Environment Factory Functions
+/* Function Prototypes for Job & Environment Factory Functions */
 Job *create_job(bool foreground, bool async);
 void push_next_job(Job *job, Job *next);
 void push_job_next_process(Job *job, Process *next_process);
