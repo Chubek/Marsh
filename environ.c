e7909153992eb502ee1bab1d096d718404ac8fb8
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-03-29 08:25:34 +0330

    Added: The Execute Command function, with IO details

--- environ.c
+++ environ.c
@@ -57,7 +57,6 @@ struct Process {
   pid_t group_id;
   Command *command;
   bool async;
-  bool piped;
   pstate_t status;
   int fno_in, fno_out, fno_err;
   Process *next;
@@ -175,4 +174,62 @@ static inline void set_job_group_id(Job *job, pid_t group_id) {
 
 static inline void set_job_terminal_attributes(Job *job, int shell_fd) {
   tcgetattr(shell_fd, &job->tmodes);
+}
+
+pid_t execute_command(Command *command, int prev_in, int *next_in) {
+  int pipe_out[2], fno_in = -1, fno_out = -1, fno_err = -1;
+  pid_t child_pid;
+
+  if (pipe(pipe_out) < 0) {
+    runtime_error(RTMERR_NONFATAL | RTMERR_IO, "Failed to create pipe");
+    ABORT_FORK();
+  }
+
+  if ((child_pid = fork) < 0) {
+    runtime_error(RTMERR_NONFATAL | RTMERR_PROCESS,
+                  "Failed to fork the parent process");
+    ABORT_FORK();
+
+  } else if (!child_pid) {
+    if (command->redir != NULL) {
+      if (hook_redir(command->redir, &fno_in, &fno_out, &fno_err) == -1) {
+        runtime_error(RTMERR_NONFATAL | RTMERR_IO, "Failed to redirect output");
+        ABORT_EXEC();
+      }
+    }
+
+    if (fno_in == -1 && prev_in == -1)
+      fno_in = STDIN_FILENO;
+    else if (fno_in == -1 && prev_in > 0)
+      fno_in = prev_in;
+
+    if (next_in != NULL) {
+      if (dup2(pipe_out[1], fno_out) < 0) {
+        runtime_error(RTMERR_NONFATAL | RTMERR_IO, "Failed to pipe output");
+        ABORT_EXEC();
+      }
+
+      *next_in = pipe_out[0];
+      close(pipe_out[0]);
+      close(pipe_out[1]);
+    }
+
+    if (command->arguments[command->num_arguments - 1] != NULL) {
+      command->arguments = (char **)command_realloc(
+          command->arguments, (command->num_arguments + 1) * sizeof(char *));
+      command->arguments[command->num_arguments++] = NULL;
+    }
+
+    execlp(command->util_name, command->arguments);
+    runtime_error(RTMERR_FATAL | RTERR_EXEC, "Execution failed");
+    ABORT_POSTEXEC();
+  }
+
+  close(pipe_out[0]);
+  close(pipe_out[1]);
+
+  if (prev_in != -1)
+    close(prev_in);
+
+  return child_pid;
 }
