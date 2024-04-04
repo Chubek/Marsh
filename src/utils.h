710c0c096038fad09f5887e6992aaa58d586d6cd
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-04-04 23:13:28 +0330

    Added: prototype declarations for symbol and symbols table

--- src/utils.h
+++ src/utils.h
@@ -5,11 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum SymbolKind symkind_t;
+
 typedef struct Arena Arena;
+typedef struct Symtbl Symtbl;
+typedef struct Symbol;
 
 Arena *arena_init(size_t size);
 void *arena_alloc(Arena *arena, size_t size);
 void arena_reset(Arena *arena);
 void arena_free(Arena *arena);
+
+
+Symtbl *symtbl_init(size_t size);
+void symtbl_insert(Symbol *sym, char *key, symkind_t kind);
+Symbol *symtbl_retrieve(char *key, symkind_t kind);
+
 
 #endif
