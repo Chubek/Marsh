335b3defc037dbf9f5e23187a37f4fc2fb2c8c2b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-04-06 18:17:04 +0330

    Major Revision: End to reliancy of files on paired header files, now all declarations are in `include/marsh.h`

--- include/marsh.h
+++ include/marsh.h
@@ -5,8 +5,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// TODO: Implement
+typedef String String;
+typedef Arena Arena;
+typedef ProcessIO ProcessIO;
 
+void ProcessIO_init(ProcessIO* pio, struct Arena* arena);
+void ProcessIO_redirect_in(ProcessIO* pio, String* file_path);
+void ProcessIO_redirect_out(ProcessIO* pio, String* file_path);
+void ProcessIO_redirect_err(ProcessIO* pio, String* file_path);
+void ProcessIO_cleanup(ProcessIO* pio); 
 
 
 
